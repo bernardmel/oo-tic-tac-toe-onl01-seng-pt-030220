0e0b5f9883d507e55ad19345df19a7f71f559b38
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-24 18:44:19 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,7 +27,9 @@ end
     user_input.to_i - 1
   end
 
-  
+  def move(index, current_player = "X")
+    @board[index] = current_player
+  end
 
 def position_taken?(index)
   if @board[index] == "X" || @board[index] == "0"
